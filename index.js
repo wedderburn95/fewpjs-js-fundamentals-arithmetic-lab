00b936581b1290c14b7502542b072a7a834a96cb
william <wedderburn95@yahoo.com>
2021-05-20 02:33:48 -0400

    commit tree 50e78cd8210df0978f4f02e2675ad69b33b13313

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+const random = Math.ceil(0.5)
+const num3 = 3;
+const num4 = 1;
+const mod = num3 + num4;
+const max =Math.max(10, 18, 20);
